9cfe502fed014b693cc5ac7f1aa45dc9f333eb63
gena <genagreiner94@gmail.com>
2023-01-13 18:49:38 +0200

    feat: add base html structure

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,2 @@
-console.log('hello from ts!');
-function s() {
-    fetch('http://localhost:3000/garage')
-        .then((res) => res.json())
-        .then((res) => console.log(res));
-}
-s();
+import '../assets/scss/style.scss';
+import '../index.html';
--- webpack.config.js
+++ webpack.config.js
@@ -8,95 +8,95 @@ const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
-  mode,
-  target,
-  devtool,
-  devServer: {
-    port: 4000,
-    open: true,
-    hot: true,
-  },
-  entry: {
-    index: path.resolve(__dirname, './src/ts/index')
-  },
-  output: {
-    filename: '[name].js',
-    clean: true,
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[name][ext]'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-          test: /\.(c|sa|sc)ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port: 4000,
+        open: true,
+        hot: true,
+    },
+    entry: {
+        index: path.resolve(__dirname, './src/ts/index')
+    },
+    output: {
+        filename: '[name].js',
+        clean: true,
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
             {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: [require('postcss-preset-env')],
-                },
-              },
+                test: /\.html$/i,
+                loader: 'html-loader',
             },
-            'sass-loader',
-          ],
-      },
-      {
-        test: /\.woff2?$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]'
-        }
-      },
-      {
-        test: /\.(jpe?g|png|webp|gif|svg)$/i,
-        use: [
-          {
-            loader: 'img-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
-              },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(ttf|woff2?)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+            {
+                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'img-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            },
+                        }
+                    }
+                ],
+                type: 'asset/resource',
+            },
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             }
-          }
         ],
-        type: 'asset/resource',
-      },
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+            chunks: ['index']
+        }),
+        new CleanWebpackPlugin()
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
-      chunks: ['index']
-    }),
-    new CleanWebpackPlugin()
-  ],
 };
